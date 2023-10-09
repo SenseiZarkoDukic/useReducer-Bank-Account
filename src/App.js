7cd88d2afcdcc53fed131d72ba21f5c7d142ee06
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-10-09 18:27:47 +0200

    Set disabled of openAccount isActive state

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ export default function App() {
           onClick={() => {
             dispatch({ type: "openAccount" });
           }}
-          disabled={false}
+          disabled={isActive}
         >
           Open account
         </button>
